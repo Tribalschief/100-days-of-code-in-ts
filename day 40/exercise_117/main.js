955eb5d4c94d78421c2ed585a5a93933d5d7eb59
architectureYK <mahmedyk789@gmail.com>
2024-04-11 23:54:12 +0500

    update

---
+++ day 40/exercise_117/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let x = ["Avenger Endgame", "Zack-Snyder Justice League", "The Batman", "Spider Man No Way Home"];
+for (let i of x) {
+    console.log(i);
+}
---
+++ day 40/exercise_117/main.ts
@@ -0,0 +1,4 @@
+let movies = ["Avenger Endgame","Zack-Snyder Justice League","The Batman","Spider Man No Way Home"];
+for (let i of movies){
+    console.log(i);
+}
